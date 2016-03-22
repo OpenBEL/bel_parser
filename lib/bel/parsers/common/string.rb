062cd8e1c41aeb8059595b95dd58f91a052afef6
Anthony Bargnesi <abargnesi@selventa.com>
2016-03-22 09:27:00 -0400

    alignment of assignments

--- lib/bel/parsers/common/string.rb
+++ lib/bel/parsers/common/string.rb
@@ -130,9 +130,9 @@ self.bel_en_string = 1;
 
           def each
             @buffers = {}
-            data = @content.unpack('C*')
-            p   = 0
-            pe  = data.length
+            data     = @content.unpack('C*')
+            p        = 0
+            pe       = data.length
 
       # begin: ragel        
             
