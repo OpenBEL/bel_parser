c65d03e170613b80b7a20a020b5efa5bb70746ce
Anthony Bargnesi <abargnesi@selventa.com>
2016-03-30 14:47:13 -0400

    added version2 return type test; cheap copy
    refs #19

---
+++ spec/unit/language/version2/return_type_spec.rb
@@ -0,0 +1,58 @@
+require 'bel/language/version2'
+
+describe BEL::Language::Version2::ReturnTypes do
+  RETURN_TYPES = BEL::Language::Version2::ReturnTypes.constants.map do |const|
+    BEL::Language::Version2::ReturnTypes.const_get(const)
+  end
+
+  RETURN_TYPES.each do |return_type|
+    type_name = return_type.name.split('::')[-1]
+    describe type_name do
+      let(:any_type) do
+        BEL::Language::Version2::ReturnTypes::Any
+      end
+
+      it 'provides symbol value' do
+        expect(return_type).to respond_to(:to_sym)
+      end
+
+      it 'provides its subtypes' do
+        expect(return_type).to respond_to(:subtype_of?)
+      end
+
+      it 'provides all subtypes, transitively' do
+        expect(return_type).to respond_to(:transitive_subtypes)
+      end
+
+      it 'is a subtype of itself' do
+        expect(return_type.subtype_of?(return_type)).to be true
+      end
+
+      it 'is a subtype of Any' do
+        expect(return_type.subtype_of?(any_type)).to be true
+      end
+
+      it 'all subtypes are subtype_of? me' do
+        expect(return_type.subtypes).to all(
+          satisfy("be subtypes of #{type_name}") do |type|
+            type.subtype_of?(return_type)
+          end
+        )
+      end
+
+      context 'when comparing return type Classes' do
+        it 'is less than or equal to Any' do
+          expect(return_type).to be <= any_type
+        end
+
+        it 'Any is greater than or equal to me' do
+          expect(any_type).to be >= return_type
+        end
+
+        it 'is same class' do
+          expect(return_type).to be == return_type
+        end
+      end
+    end
+  end
+end
