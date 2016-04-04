6e506e3e310bae62c19d018e29bdc5c526ebfd44
Anthony Bargnesi <abargnesi@selventa.com>
2016-04-04 15:45:10 -0400

    more style fixes

--- convert_signatures.rb
+++ convert_signatures.rb
@@ -12,7 +12,7 @@ class ArgBuilder
   include BEL::Language::Semantics::Builder
 
   def build(&block)
-    self.instance_eval(&block)
+    instance_eval(&block)
     @arg
   end
 
@@ -25,7 +25,7 @@ class ArgBuilder
   end
 end
 
-def term_sexp(function_identifier, spec)
+def term_sexp(function_identifier, _spec)
   function = V2.function(function_identifier.to_sym)
   BEL::Language::Semantics::Builder.build do
     term(
@@ -103,24 +103,24 @@ def ast(string_form)
       encoding[0] = encoding[0].upcase
       ast = ast << arg_parameter_sexp(encoding)
     when /E:[*]/
-      ast = ast  << arg_wild_parameter_sexp
+      ast = ast << arg_wild_parameter_sexp
     when /F:([a-zA-Z]+)\.\.\./
-      return_type  = Regexp.last_match[1]
-      ast = ast   << variadic_function_return_type_sexp(return_type, V2)
+      return_type = Regexp.last_match[1]
+      ast = ast << variadic_function_return_type_sexp(return_type, V2)
     when /F:([a-zA-Z]+)/
-      return_type  = Regexp.last_match[1]
-      ast = ast   << arg_function_return_type_sexp(return_type, V2)
+      return_type = Regexp.last_match[1]
+      ast = ast << arg_function_return_type_sexp(return_type, V2)
     end
   end
   ast
 end
 
-OUT_DIR = 'lib/bel/language/version2/gen_signatures'
+OUT_DIR = 'lib/bel/language/version2/gen_signatures'.freeze
 template = ERB.new(File.read('semantic_template.erb'))
-Dir.mkdir(OUT_DIR) unless Dir.exists?(OUT_DIR)
+Dir.mkdir(OUT_DIR) unless Dir.exist?(OUT_DIR)
 VERSION1_SIGNATURES.each do |data|
   name, description, string_form = data.values_at(:name, :description, :string_form)
-  ast = ast(string_form).inspect(6).gsub('-','_').gsub(/\(([a-zA-Z_]+$)/, '\1(')
+  ast = ast(string_form).inspect(6).tr('-', '_').gsub(/\(([a-zA-Z_]+$)/, '\1(')
   ast.gsub!(/(s\(:[a-zA-Z_]+), ?/) do |type|
     type = type.match(/:([a-zA-Z_]+)/)[1]
     "#{type}("
@@ -138,22 +138,22 @@ VERSION1_SIGNATURES.each do |data|
     .gsub(/([A-Z])(?=[a-z])/) { |letter| "_#{letter.downcase}" }[1..-1]
     .gsub(/([A-Z]+)/) { |letters| "_#{letters.downcase}" }
   file_name = case file_name
-  when "_na_abundance"
-    "rna_abundance"
-  when "_na_abundance_with_fusion"
-    "rna_abundance_with_fusion"
-  when "_tp_bound_activity_of_complex_abundance"
-    "gtp_bound_activity_of_complex_abundance"
-  when "_tp_bound_activity_of_protein_abundance"
-    "gtp_bound_activity_of_protein_abundance"
-  when "_na_abundance_with_location"
-    "rna_abundance_with_location"
-  when "_na_abundance_with_variant"
-    "rna_abundance_with_variant"
-  else
-    file_name
-	end
+              when '_na_abundance'
+                'rna_abundance'
+              when '_na_abundance_with_fusion'
+                'rna_abundance_with_fusion'
+              when '_tp_bound_activity_of_complex_abundance'
+                'gtp_bound_activity_of_complex_abundance'
+              when '_tp_bound_activity_of_protein_abundance'
+                'gtp_bound_activity_of_protein_abundance'
+              when '_na_abundance_with_location'
+                'rna_abundance_with_location'
+              when '_na_abundance_with_variant'
+                'rna_abundance_with_variant'
+              else
+                file_name
+  end
   File.open("lib/bel/language/version2/gen_signatures/#{file_name}.rb", 'w') do |f|
     f.write(contents)
-	end
+  end
 end
