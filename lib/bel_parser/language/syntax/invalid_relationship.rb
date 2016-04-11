d0f6494981b92a76e407a3c8292275ada0e8c2b0
Anthony Bargnesi <abargnesi@selventa.com>
2016-04-11 16:05:42 -0400

    added SyntaxFunction for invalid relationships

---
+++ lib/bel_parser/language/syntax/invalid_relationship.rb
@@ -0,0 +1,42 @@
+require 'bel_parser/parsers/ast/node'
+
+module BELParser
+  module Language
+    module Syntax
+      # InvalidRelationship represents a syntax error with invalid
+      # relationship according to a BEL specification.
+      class InvalidRelationship
+        include SyntaxFunction
+
+        private_class_method :new
+
+        def self.map(stmt_node, spec, namespaces)
+          return nil unless stmt_node.is_a?(BELParser::Parsers::AST::Statement)
+
+          rel_name = stmt_node.relationship.string_literal
+          return nil if rel_name.nil?
+
+          unless spec.relationship(rel_name.to_sym)
+            InvalidRelationshipSyntaxError.new(stmt_node, spec, rel_name)
+          end
+        end
+      end
+
+      # InvalidRelationshipSyntaxError indicates a relationship was invalid.
+      class InvalidRelationshipSyntaxError < SyntaxError
+        # Gets the relationship literal that was invalid according to a
+        # BEL specification.
+        attr_reader :relationship
+
+        def initialize(stmt_node, spec, relationship)
+          super(stmt_node, spec)
+          @relationship = relationship
+        end
+
+        def msg
+          %Q{Invalid relationship "#{relationship}".}
+        end
+      end
+    end
+  end
+end
