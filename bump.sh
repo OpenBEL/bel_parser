b79d8c100641aa74d3808e4bd4d7d0f9e3769f39
Anthony Bargnesi <abargnesi@selventa.com>
2016-04-13 10:24:01 -0400

    automate next version in bump script; confirms

--- bump.sh
+++ bump.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
 
-VERSION=$1
+CURRENT=$(cat VERSION)
+NEXT=$(echo "puts \"${CURRENT}\".next" | ruby)
 
-if [ -z $VERSION ]; then
-    echo "usage: bump.sh [NEXT GEM VERSION]" >&2
+read -p "Bump and release $NEXT [Y OR y]? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo -e "\nExiting."
     exit 1
 fi
 
 # set version in VERSION file
-echo "$VERSION" > VERSION
+echo "$NEXT" > VERSION
 
 # update ISO 8601 date in gemspec
 sed -ri "s/[0-9]{4}-[0-9]{2}-[0-9]{2}/$(date --iso-8601)/g" .gemspec
 
 # commit
-git commit .gemspec VERSION -m "Bumped to version $VERSION."
+git commit .gemspec VERSION -m "Bumped to version $NEXT."
 
 # tag as VERSION
-git tag -a "$VERSION" -m "Tagged as version $VERSION."
+git tag -a "$NEXT" -m "Tagged as version $NEXT."
 
 # push commits upstream
 REF=$(git symbolic-ref HEAD); git push upstream $REF
 
 # push tag upstream
-REF=$(git symbolic-ref HEAD); git push upstream "$VERSION"
+REF=$(git symbolic-ref HEAD); git push upstream "$NEXT"
 
 # build gem
 gem build .gemspec
 
 # push gem
-gem push "bel_parser-$VERSION.gem"
+gem push "bel_parser-$NEXT.gem"
