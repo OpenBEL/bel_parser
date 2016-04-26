37f1a79e836f4a74f1b2292f52278fb2dde62e10
Anthony Bargnesi <abargnesi@selventa.com>
2016-04-26 16:54:18 -0400

    added protein modification to 2.0 value encodings
    refs #28

---
+++ lib/bel_parser/language/version2_0/value_encodings/protein_modification.rb
@@ -0,0 +1,21 @@
+require_relative '../../version2_0'
+require_relative 'any'
+
+module BELParser
+  module Language
+    module Version2_0
+      module ValueEncodings
+        # ProteinModification value encoding.
+        class ProteinModification < Any
+          # Return the {Symbol} value.
+          #
+          # @note This method should be overridden in subclasses.
+          def self.to_sym
+            raise_not_implemented(__method__) if self != ProteinModification
+            :E
+          end
+        end
+      end
+    end
+  end
+end
