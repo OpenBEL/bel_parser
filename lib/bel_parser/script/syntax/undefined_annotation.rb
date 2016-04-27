0668190107c5822812c2e45b20cbeb841b70aa0a
Anthony Bargnesi <abargnesi@selventa.com>
2016-04-27 11:16:36 -0400

    syntax function for undefined annotations
    This results in a syntax error when a SET record references an undefined
    annotation.
    
    refs #42

---
+++ lib/bel_parser/script/syntax/undefined_annotation.rb
@@ -0,0 +1,61 @@
+require 'bel_parser/language'
+require 'bel_parser/language/syntax_function'
+require 'bel_parser/language/syntax_error'
+require 'bel_parser/quoting'
+require 'bel_parser/parsers/ast/node'
+require 'concurrent/hash'
+
+module BELParser
+  module Script
+    module Syntax
+      class UndefinedAnnotation
+        extend BELParser::Language::Syntax::SyntaxFunction
+
+        TARGET_NODE = BELParser::Parsers::AST::Set
+
+        def self.map(ast_node, script_context)
+          return nil unless ast_node.is_a?(TARGET_NODE)
+          prefix = ast_node.name.identifier.string_literal
+          annotation_definitions =
+            script_context[:annotation_definitions] ||= Concurrent::Hash.new
+
+          if annotation_definitions.key?(prefix)
+            nil
+          else
+            UndefinedAnnotationError.new(
+              ast_node,
+              prefix,
+              script_context[:annotation_definitions])
+          end
+        end
+      end
+
+      # UndefinedAnnotationError represents an undefined annotation seen when
+      # checking a SET annotation.
+      class UndefinedAnnotationError < BELParser::Language::Syntax::SyntaxError
+        # Gets the undefined prefix.
+        attr_reader :prefix
+
+        def initialize(set_node, prefix, annotation_definitions)
+          super(set_node, nil)
+          @prefix                 = prefix
+          @annotation_definitions = annotation_definitions
+        end
+
+        def msg
+          defined_annotations =
+            if @annotation_definitions.empty?
+              'No annotations are defined.'
+            else
+              annotation_prefixes = @annotation_definitions.keys.join(', ')
+              "Defined annotations: #{annotation_prefixes}"
+            end
+          <<-MSG.gsub(/^ +/, '').delete("\n")
+            Annotation definition is missing for "#@prefix". 
+            #{defined_annotations}
+          MSG
+        end
+      end
+    end
+  end
+end
