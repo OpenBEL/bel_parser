a7a13490c0385ce4f6785097fd85f9d48ba37c3e
Anthony Bargnesi <abargnesi@selventa.com>
2016-04-27 22:24:24 -0400

    unquote bel_version string literal for spec lookup

--- lib/bel_parser/script/state/bel_version.rb
+++ lib/bel_parser/script/state/bel_version.rb
@@ -1,5 +1,6 @@
 require 'bel_parser/language'
 require 'bel_parser/parsers/ast/node'
+require 'bel_parser/quoting'
 require 'concurrent/hash'
 require_relative '../state_function'
 
@@ -8,6 +9,7 @@ module BELParser
     module State
       class BELVersion
         extend StateFunction
+        extend BELParser::Quoting
 
         TARGET_NODE         = BELParser::Parsers::AST::DocumentProperty
         BEL_VERSION_REGEX   = /#{Regexp.escape('bel_version')}/i
@@ -19,7 +21,7 @@ module BELParser
           name_string  = name.identifier.string_literal
           return unless name_string =~ BEL_VERSION_REGEX
 
-          value_string = value.children[0].string_literal
+          value_string = unquote(value.children[0].string_literal)
           begin
             spec = BELParser::Language.specification(value_string)
           rescue ArgumentError
