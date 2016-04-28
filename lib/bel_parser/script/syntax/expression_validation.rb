4eacaacc05eb7d514c558a63805ccf78a73b6ae9
Anthony Bargnesi <abargnesi@selventa.com>
2016-04-27 23:27:05 -0400

    add expression validation as a BEL Script check
    This SyntaxFunction executes both syntax/semantic checks for expressions
    by directly using the BELParser::Language::ExpressionValidator.
    
    refs #42

---
+++ lib/bel_parser/script/syntax/expression_validation.rb
@@ -0,0 +1,46 @@
+require 'bel_parser/language'
+require 'bel_parser/language/expression_validator'
+require 'bel_parser/language/syntax_function'
+require 'bel_parser/language/syntax_warning'
+require 'bel_parser/quoting'
+require 'bel_parser/parsers/ast/node'
+require 'concurrent/hash'
+
+module BELParser
+  module Script
+    module Syntax
+      class ExpressionValidation
+        extend BELParser::Language::Syntax::SyntaxFunction
+        extend BELParser::Quoting
+
+        TARGET_NODES = [
+          BELParser::Parsers::AST::ObservedTerm,
+          BELParser::Parsers::AST::SimpleStatement,
+          BELParser::Parsers::AST::NestedStatement
+        ]
+        EXP_VALIDATOR = BELParser::Language::ExpressionValidator
+
+        def self.map(ast_node, script_context)
+          return nil unless TARGET_NODES.include?(ast_node.class)
+          return nil unless script_context.key?(:specification)
+
+          validator = expression_validator(script_context)
+          validator.validate(ast_node).select(&:failure?)
+        end
+
+        def self.expression_validator(script_context)
+          unless defined? @expression_validator
+            spec, namespaces, reader =
+              script_context
+              .values_at(
+                :specification,
+                :namespace_definitions,
+                :resource_reader)
+            @expression_validator = EXP_VALIDATOR.new(spec, namespaces, reader)
+          end
+          @expression_validator
+        end
+      end
+    end
+  end
+end
