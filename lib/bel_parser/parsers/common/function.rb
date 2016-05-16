fe7a82ec15a025c3e154bbf58d5e2ce578c3530b
Nick Bargnesi <nbargnesi@selventa.com>
2016-05-15 23:21:53 -0400

    add function machine

---
+++ lib/bel_parser/parsers/common/function.rb
@@ -0,0 +1,312 @@
+
+# begin: ragel
+=begin
+
+
+=end
+# end: ragel
+
+require_relative '../ast/node'
+require_relative '../mixin/buffer'
+require_relative '../nonblocking_io_wrapper'
+
+module BELParser
+  module Parsers
+    module Common
+      module Function
+
+        class << self
+
+          MAX_LENGTH = 1024 * 128 # 128K
+
+          def parse(content)
+            return nil unless content
+
+            Parser.new(content).each do |obj|
+              yield obj
+            end
+          end
+        end
+
+        private
+
+        class Parser
+          include Enumerable
+          include BELParser::Parsers::Buffer
+          include BELParser::Parsers::AST::Sexp
+
+          def initialize(content)
+            @content = content
+      # begin: ragel
+            
+class << self
+	attr_accessor :_bel_trans_keys
+	private :_bel_trans_keys, :_bel_trans_keys=
+end
+self._bel_trans_keys = [
+	0, 0, 65, 122, 10, 122, 
+	0, 0, 0
+]
+
+class << self
+	attr_accessor :_bel_key_spans
+	private :_bel_key_spans, :_bel_key_spans=
+end
+self._bel_key_spans = [
+	0, 58, 113, 0
+]
+
+class << self
+	attr_accessor :_bel_index_offsets
+	private :_bel_index_offsets, :_bel_index_offsets=
+end
+self._bel_index_offsets = [
+	0, 0, 59, 173
+]
+
+class << self
+	attr_accessor :_bel_indicies
+	private :_bel_indicies, :_bel_indicies=
+end
+self._bel_indicies = [
+	1, 1, 1, 1, 1, 1, 1, 1, 
+	1, 1, 1, 1, 1, 1, 1, 1, 
+	1, 1, 1, 1, 1, 1, 1, 1, 
+	1, 1, 0, 0, 0, 0, 0, 0, 
+	1, 1, 1, 1, 1, 1, 1, 1, 
+	1, 1, 1, 1, 1, 1, 1, 1, 
+	1, 1, 1, 1, 1, 1, 1, 1, 
+	1, 1, 0, 2, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 0, 0, 0, 0, 0, 0, 
+	0, 0, 3, 3, 3, 3, 3, 3, 
+	3, 3, 3, 3, 3, 3, 3, 3, 
+	3, 3, 3, 3, 3, 3, 3, 3, 
+	3, 3, 3, 3, 0, 0, 0, 0, 
+	0, 0, 3, 3, 3, 3, 3, 3, 
+	3, 3, 3, 3, 3, 3, 3, 3, 
+	3, 3, 3, 3, 3, 3, 3, 3, 
+	3, 3, 3, 3, 0, 4, 0
+]
+
+class << self
+	attr_accessor :_bel_trans_targs
+	private :_bel_trans_targs, :_bel_trans_targs=
+end
+self._bel_trans_targs = [
+	0, 2, 3, 2, 0
+]
+
+class << self
+	attr_accessor :_bel_trans_actions
+	private :_bel_trans_actions, :_bel_trans_actions=
+end
+self._bel_trans_actions = [
+	1, 2, 4, 0, 0
+]
+
+class << self
+	attr_accessor :_bel_eof_actions
+	private :_bel_eof_actions, :_bel_eof_actions=
+end
+self._bel_eof_actions = [
+	0, 1, 3, 5
+]
+
+class << self
+	attr_accessor :bel_start
+end
+self.bel_start = 1;
+class << self
+	attr_accessor :bel_first_final
+end
+self.bel_first_final = 2;
+class << self
+	attr_accessor :bel_error
+end
+self.bel_error = 0;
+
+class << self
+	attr_accessor :bel_en_function_node
+end
+self.bel_en_function_node = 1;
+
+
+      # end: ragel
+          end
+
+          def each
+            @buffers    = {}
+            @incomplete = {}
+            data        = @content.unpack('C*')
+            p           = 0
+            p_start     = 0
+            p_end       = 0
+            pe          = data.length
+            eof         = data.length
+            @function_started = false
+
+      # begin: ragel
+            
+begin
+	p ||= 0
+	pe ||= data.length
+	cs = bel_start
+end
+
+            
+begin
+	testEof = false
+	_slen, _trans, _keys, _inds, _acts, _nacts = nil
+	_goto_level = 0
+	_resume = 10
+	_eof_trans = 15
+	_again = 20
+	_test_eof = 30
+	_out = 40
+	while true
+	if _goto_level <= 0
+	if p == pe
+		_goto_level = _test_eof
+		next
+	end
+	if cs == 0
+		_goto_level = _out
+		next
+	end
+	end
+	if _goto_level <= _resume
+	_keys = cs << 1
+	_inds = _bel_index_offsets[cs]
+	_slen = _bel_key_spans[cs]
+	_wide = data[p].ord
+	_trans = if (   _slen > 0 && 
+			_bel_trans_keys[_keys] <= _wide && 
+			_wide <= _bel_trans_keys[_keys + 1] 
+		    ) then
+			_bel_indicies[ _inds + _wide - _bel_trans_keys[_keys] ] 
+		 else 
+			_bel_indicies[ _inds + _slen ]
+		 end
+	cs = _bel_trans_targs[_trans]
+	if _bel_trans_actions[_trans] != 0
+	case _bel_trans_actions[_trans]
+	when 2 then
+		begin
+
+    $stderr.puts 'FUNCTION start_function'
+    @function_started = true
+    p_start = p;
+  		end
+	when 4 then
+		begin
+
+    $stderr.puts 'FUNCTION end_function'
+    p_end = p
+    chars = data[p_start...p_end]
+    completed = !chars.empty?
+    ident_node = identifier(utf8_string(chars), complete: completed)
+    fx_node = function(ident_node, complete: ident_node.complete)
+    @buffers[:function] = fx_node
+  		end
+	when 1 then
+		begin
+
+    $stderr.puts 'FUNCTION function_node_err'
+    if @function_started
+      # hit invalid char, include it in the identifier that results
+      p_end = p + 1
+      chars = data[p_start...p_end]
+      completed = !chars.empty?
+      ident_node = identifier(utf8_string(chars), complete: completed)
+      fx_node = function(ident_node, complete: false)
+      @buffers[:function] = fx_node
+      yield @buffers[:function]
+    end
+  		end
+	end
+	end
+	end
+	if _goto_level <= _again
+	if cs == 0
+		_goto_level = _out
+		next
+	end
+	p += 1
+	if p != pe
+		_goto_level = _resume
+		next
+	end
+	end
+	if _goto_level <= _test_eof
+	if p == eof
+	  case _bel_eof_actions[cs]
+	when 1 then
+		begin
+
+    $stderr.puts 'FUNCTION function_node_err'
+    if @function_started
+      # hit invalid char, include it in the identifier that results
+      p_end = p + 1
+      chars = data[p_start...p_end]
+      completed = !chars.empty?
+      ident_node = identifier(utf8_string(chars), complete: completed)
+      fx_node = function(ident_node, complete: false)
+      @buffers[:function] = fx_node
+      yield @buffers[:function]
+    end
+  		end
+	when 5 then
+		begin
+
+    $stderr.puts 'FUNCTION yield_function'
+    yield @buffers[:function]
+  		end
+	when 3 then
+		begin
+
+    $stderr.puts 'FUNCTION end_function'
+    p_end = p
+    chars = data[p_start...p_end]
+    completed = !chars.empty?
+    ident_node = identifier(utf8_string(chars), complete: completed)
+    fx_node = function(ident_node, complete: ident_node.complete)
+    @buffers[:function] = fx_node
+  		end
+		begin
+
+    $stderr.puts 'FUNCTION yield_function'
+    yield @buffers[:function]
+  		end
+	  end
+	end
+
+	end
+	if _goto_level <= _out
+		break
+	end
+end
+	end
+
+      # end: ragel
+          end
+        end
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  $stdin.each_line do |line|
+    BELParser::Parsers::Common::Function.parse(line) { |obj|
+      puts obj.inspect
+    }
+  end
+end
+
+# vim: ft=ruby ts=2 sw=2:
+# encoding: utf-8
---
+++ lib/bel_parser/parsers/common/function.rl
@@ -0,0 +1,147 @@
+# begin: ragel
+=begin
+%%{
+  machine bel;
+
+  include 'common.rl';
+
+  action start_function {
+    $stderr.puts 'FUNCTION start_function'
+    @function_started = true
+    p_start = p;
+  }
+
+  action end_function {
+    $stderr.puts 'FUNCTION end_function'
+    p_end = p
+    chars = data[p_start...p_end]
+    completed = !chars.empty?
+    ident_node = identifier(utf8_string(chars), complete: completed)
+    fx_node = function(ident_node, complete: ident_node.complete)
+    @buffers[:function] = fx_node
+  }
+
+  action a_function_eof {
+    $stderr.puts 'FUNCTION a_function_eof'
+    if @function_started
+      p_end = p
+      chars = data[p_start...p_end]
+      completed = !chars.empty?
+      ident_node = identifier(utf8_string(chars), complete: completed)
+      fx_node = function(ident_node, complete: ident_node.complete)
+      @buffers[:function] = fx_node
+    end
+  }
+
+  action function_node_err {
+    $stderr.puts 'FUNCTION function_node_err'
+    if @function_started
+      # hit invalid char, include it in the identifier that results
+      p_end = p + 1
+      chars = data[p_start...p_end]
+      completed = !chars.empty?
+      ident_node = identifier(utf8_string(chars), complete: completed)
+      fx_node = function(ident_node, complete: false)
+      @buffers[:function] = fx_node
+      yield @buffers[:function]
+    end
+  }
+
+  action yield_function {
+    $stderr.puts 'FUNCTION yield_function'
+    yield @buffers[:function]
+  }
+
+  function =
+    FUNCTION
+    >start_function
+    %end_function
+    ;
+
+  maybe_function =
+    function?
+    ;
+
+  a_function =
+    function
+    $eof(a_function_eof)
+    ;
+
+  function_node :=
+    function
+    $err(function_node_err)
+    NL?
+    %yield_function
+    ;
+}%%
+=end
+# end: ragel
+
+require_relative '../ast/node'
+require_relative '../mixin/buffer'
+require_relative '../nonblocking_io_wrapper'
+
+module BELParser
+  module Parsers
+    module Common
+      module Function
+
+        class << self
+
+          MAX_LENGTH = 1024 * 128 # 128K
+
+          def parse(content)
+            return nil unless content
+
+            Parser.new(content).each do |obj|
+              yield obj
+            end
+          end
+        end
+
+        private
+
+        class Parser
+          include Enumerable
+          include BELParser::Parsers::Buffer
+          include BELParser::Parsers::AST::Sexp
+
+          def initialize(content)
+            @content = content
+      # begin: ragel
+            %% write data;
+      # end: ragel
+          end
+
+          def each
+            @buffers    = {}
+            @incomplete = {}
+            data        = @content.unpack('C*')
+            p           = 0
+            p_start     = 0
+            p_end       = 0
+            pe          = data.length
+            eof         = data.length
+            @function_started = false
+
+      # begin: ragel
+            %% write init;
+            %% write exec;
+      # end: ragel
+          end
+        end
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  $stdin.each_line do |line|
+    BELParser::Parsers::Common::Function.parse(line) { |obj|
+      puts obj.inspect
+    }
+  end
+end
+
+# vim: ft=ruby ts=2 sw=2:
+# encoding: utf-8
