2d23872d67a80e92756747beee7aa054af34f90f
Anthony Bargnesi <abargnesi@selventa.com>
2016-05-27 13:02:42 -0400

    set up Namespace objects for DEFINE Namespace...

--- lib/bel_parser/script/state/namespace_definition.rb
+++ lib/bel_parser/script/state/namespace_definition.rb
@@ -1,6 +1,7 @@
 require 'bel_parser/language'
-require 'bel_parser/parsers/ast/node'
-require 'concurrent/hash'
+require          'concurrent/hash'
+require          'bel_parser/parsers/ast/node'
+require          'bel_parser/expression/model/namespace'
 require_relative '../state_function'
 require_relative '../apply_resource_uri'
 
@@ -18,25 +19,37 @@ module BELParser
           url_reader = script_context[:url_reader]
 
           keyword, domain = ast_node.children
-          prefix          = keyword.identifier.string_literal
+          keyword_s       = keyword.identifier.string_literal
 
           case
           when domain.uri?
             uri     = domain.child.string.string_literal
             dataset = uri_reader.retrieve_resource(uri)
             script_context[:namespace_definitions] ||= Concurrent::Hash.new
-            script_context[:namespace_definitions][prefix] = [:uri, dataset]
+            script_context[:namespace_definitions][keyword_s] =
+              BELParser::Expression::Model::Namespace.new(
+                keyword_s,
+                uri,
+                nil)
           when domain.url?
             ApplyResourceURI.new.process(ast_node)
             if ast_node.uri
               dataset = uri_reader.retrieve_resource(ast_node.uri)
               script_context[:namespace_definitions] ||= Concurrent::Hash.new
-              script_context[:namespace_definitions][prefix] = [:uri, dataset]
+              script_context[:namespace_definitions][keyword_s] =
+                BELParser::Expression::Model::Namespace.new(
+                  keyword_s,
+                  ast_node.uri,
+                  domain.child.string.string_literal)
             else
               url     = domain.child.string.string_literal
               dataset = url_reader.retrieve_resource(url)
               script_context[:namespace_definitions] ||= Concurrent::Hash.new
-              script_context[:namespace_definitions][prefix] = [:url, dataset]
+              script_context[:namespace_definitions][keyword_s] =
+                BELParser::Expression::Model::Namespace.new(
+                  keyword_s,
+                  nil,
+                  url)
             end
           end
         end
